224c1fc805808afff3b9c21fc23e7937db48fb1e
Pavle Josic <pavlejosic2004@gmail.com>
2025-01-18 23:41:28 +0100

    Now we can type in InputBox :)

--- items/inputbox.py
+++ items/inputbox.py
@@ -4,6 +4,25 @@ from rich.panel import Panel
 
 class InputBox(Item):
     
-    def __init__(self, x, y):
-        super().__init__(x, y)
-        self.printable = Panel("")
+    def __init__(self, tw, th):
+        super().__init__(0, th-3)
+        self.buffer = ""
+        self.printable = Panel(self.buffer + "[#999999]|")
+        print(tw)
+        self.max_buffer_len = tw-5
+        
+    def type_in(self, char, term):
+        self.buffer += char
+        self.update_buffer(term)
+        
+    def delete(self, term):
+        self.buffer = self.buffer[:-1]
+        self.update_buffer(term)    
+        
+    def update_buffer(self, term):
+        if len(self.buffer) > self.max_buffer_len:
+            self.printable = Panel(self.buffer[len(self.buffer)-self.max_buffer_len:] + "[red]|")
+        else:
+            self.printable = Panel(self.buffer + "[red]|")
+        self.update()
+        self.render(term)
--- render/space.py
+++ render/space.py
@@ -3,6 +3,12 @@ from items.table import Table
 from items.inputbox import InputBox
 from rich import box
 
+t = Table(5, 5, title="Table 1", box=box.ROUNDED)
+t.add_column("Col 1")
+t.add_column("Col 2")
+t.add_row("Row 11", "Row 12")
+t.add_row("Row 21", "Row 22")
+
 class Space:
     
     def __init__(self):
@@ -16,28 +22,11 @@ class Space:
             
             while True:
                 
-                key = self.term.inkey(timeout=0.5)
+                key = self.term.inkey(timeout=0.1)
 
                 if key.name == "KEY_ESCAPE":
                     break
                 
-                # For test case
-                elif key.lower() == "t":
-                    t = Table(5, 5, title="Table 1", box=box.ROUNDED)
-                    t.add_column("Col 1")
-                    t.add_column("Col 2")
-                    t.add_row("Row 11", "Row 12")
-                    t.add_row("Row 21", "Row 22")
-                    t.update()
-                    t.render(self.term)
-                    
-                elif key.lower() == "i":
-                    i = InputBox(0, self.term.height-3)
-                    i.update()
-                    i.render(self.term)    
-                    
-                elif key.lower() == "d":   
-                    i.delete(self.term)    
-                    t.delete(self.term)
+
                     
                 
